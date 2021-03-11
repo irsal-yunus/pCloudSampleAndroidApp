5136b9dde9aa6a74c9d5b7c73249fc23abec4457
irsal.yunus04 <irsal.yunus04@gmail.com>
2021-03-11 14:43:50 +0700

    update version android and gradle

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-ruby dtb.rb --email=bikee.bihari@sstsinc.com --api=stcr6bcrcx9c6ssp5mp2bdtw --did=613 --appname=old_school.android.debug__21_.apk --duration=5
+ruby dtb.rb --email=irsal.yunus04@gmail.com --api=5rbghb2hjgbcnzbsf6ty7hqb --did=613 --appname=old_school.android.debug__21_.apk --duration=5
